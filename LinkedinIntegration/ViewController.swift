6dc891feb0acb3f0ee4ebec03e3a2841d85040f3
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-09-04 17:01:07 -0700

    Feat: Connect controls with IBoutlets and IBactions.

--- LinkedinIntegration/ViewController.swift
+++ LinkedinIntegration/ViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSignIn: UIButton!
+    
+    @IBOutlet weak var btnGetProfileInfo: UIButton!
+    
+    @IBOutlet weak var btnOpenProfile: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnSignIn.enabled = true
+        btnGetProfileInfo.enabled = false
+        btnOpenProfile.hidden = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func viewDidAppear(animated: Bool) {
+    
+    
+    // MARK: IBAction Functions
+    
+    @IBAction func getProfileInfo(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func openProfileInSafari(sender: AnyObject) {
         
     }
     
---
+++ LinkedinIntegration/WebViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WebViewController.swift
+//  LinkedinIntegration
+//
+//  Created by Sugandha Naolekar on 9/4/16.
+//  Copyright © 2016 icode. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    let linkedInKey = "78jlt0emyuuvux"
+    
+    let linkedInSecret = "dZeatO9O6PqaierA"
+    
+    let authorizationEndPoint = "https://www.linkedin.com/uas/oauth2/authorization"
+    
+    let accessTokenEndPoint = "https://www.linkedin.com/uas/oauth2/accessToken"
+    
+    func startAuthorization() {
+        // Specify the response type which should always be "code".
+        let responseType = "code"
+        
+        // Set the redirect URL. Adding the percent escape characthers is necessary.
+        let redirectURL = "https://com.appcoda.linkedin.oauth/oauth".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
+        
+        // Create a random string based on the time interval (it will be in the form linkedin12345679).
+        let state = "linkedin\(Int(NSDate().timeIntervalSince1970))"
+        
+        // Set preferred scope.
+        let scope = "r_fullprofile"
+        
+    }
+
+}
